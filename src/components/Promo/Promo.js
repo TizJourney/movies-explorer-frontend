0c7796ee095f502053ad1343def8e4a2ad8e02ed
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-15 23:57:49 +0300

    добавить лого для промо страницы

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,11 +1,13 @@
 import './Promo.css';
+import promo_logo from '../../images/promo-logo.svg';
 
 import React from 'react';
 
 export default function Promo(props) {
   return (
     <div className="promo">
-      <h2>promo placeholder</h2>
+      <img className="promo__logo" src={promo_logo} alt="Лого промо страницы"/>
+      <h1 className="promo__title">Учебный проект студента веб разработки</h1>
     </div>
   )
 }
