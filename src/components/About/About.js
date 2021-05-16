0c0ee36613dedb4d966b44d9dce2fa0d00d4206a
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-16 18:40:42 +0300

    поправить классы инфо блока

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,8 +6,8 @@ export default function About(props) {
   return (
     <div className="about">
       <ul className="about-info">
-        <li className="about-info__item about-item__item_title">Дипломный проект включал 5 этапов</li>
-        <li className="about-info__item about-item__item_title">На выполнение диплома ушло 5 недель</li>
+        <li className="about-info__item about-info__item_title">Дипломный проект включал 5 этапов</li>
+        <li className="about-info__item about-info__item_title">На выполнение диплома ушло 5 недель</li>
         <li className="about-info__item">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</li>
         <li className="about-info__item">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</li>
       </ul>
