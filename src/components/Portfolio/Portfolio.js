4b1cc71d470fded7ec8b556a098a54ae96ff7c05
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-25 23:13:29 +0300

    Базовая вёрсткая для блока профиля

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,15 +4,24 @@ import React from 'react';
 
 import classnames from 'classnames';
 
+function PortofolioLink(props) {
+  return (
+    <a href={props.link} className='portfolio__link-block portfolio-link'>
+      <p className='portfolio-link__link-text'>{props.children}</p>
+      <p className='portfolio-link__link-text'>↗</p>
+    </a>
+  )
+}
+
 export default function Portofolio(props) {
   return (
     <div className={classnames('portfolio', props.className)}>
       <h3 className='portfolio__title'>Портфолио</h3>
-      <ul className='portfolio__links-blocks'>
-        <li className='link'>Статичный сайт</li>
-        <li className='link'>Адаптивный сайт</li>
-        <li className='link'>Одностраничное приложение</li>
-      </ul>
+      <div className='portfolio__links-blocks'>
+        <PortofolioLink link='https://github.com/TizJourney/mesto'>Статичный сайт</PortofolioLink>
+        <PortofolioLink link='https://github.com/TizJourney/mesto'>Адаптивный сайт</PortofolioLink>
+        <PortofolioLink link='https://github.com/TizJourney/mesto'>Одностраничное приложение</PortofolioLink>
+      </div>
     </div>
   )
 }
