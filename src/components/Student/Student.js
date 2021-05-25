1bf64be703ec07c434d301f492e719720c835212
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-25 22:13:05 +0300

    сделать правильное расположение блоков для блока студентов

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,32 +1,21 @@
 import './Student.css';
-import promo_logo from '../../images/promo-logo.svg';
-
-import { Link } from 'react-router-dom';
+import promo_logo from '../../images/student-photo.jpg';
 
 import React from 'react';
 
 export default function Student(props) {
   return (
     <div className='student'>
-      <div className='student__info student-info'>
-        <div className='student__info student-info'>
-          <div className='student-info__container'>
-            <div className='student-info__text-blocks'>
-              <h3 className='student-info__title'>Константин</h3>
-              <p className='student-info__subtitle'>Фронтенд-разработчик, 36 лет</p>
-              <p className='student-info__text'>Родился и вырос в городе Кирове. Поступил и закончил Московский Государственный Университет и с тех пор живу в Москве.</p>
-              <p className='student-info__text'>Программирование давно явлется моей профессией. В общем виде мой стаж равен около 11 лет.</p>
-              <div className='student-info__links-block'>
-                <a href='https://www.facebook.com/konstantin.bondar.7' target='_blank' rel='noreferrer' className='student-info__link'>Facebook</a>
-                <a href='https://github.com/TizJourney/' target='_blank' rel='noreferrer' className='student-info__link' >Github</a>
-              </div>
-            </div>
-            <img className="student-info__photo" src={promo_logo} alt='Фото студента' />
-          </div>
-
+      <div className='student__text-blocks'>
+        <h3 className='student__title'>Константин</h3>
+        <p className='student__subtitle'>Фронтенд-разработчик, 36 лет</p>
+        <p className='student__text'>Родился и вырос в городе Кирове. Поступил и закончил Московский Государственный Университет и с тех пор живу в Москве. Программирование давно явлется моей профессией. В общем виде мой опыт работы равен около 11 лет.</p>
+        <div className='student__links-block'>
+          <a href='https://www.facebook.com/konstantin.bondar.7' target='_blank' rel='noreferrer' className='student__link'>Facebook</a>
+          <a href='https://github.com/TizJourney/' target='_blank' rel='noreferrer' className='student__link' >Github</a>
         </div>
       </div>
-
+      <img className="student__photo" src={promo_logo} alt='Фото студента' />
     </div>
   )
 }
