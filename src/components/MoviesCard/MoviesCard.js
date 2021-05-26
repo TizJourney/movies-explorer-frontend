4d64bdf7e7978a7edc01f14933496bea509cb6d2
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-26 18:42:33 +0300

    сделать базовую функциональность по сохранению карточек

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,9 +2,14 @@ import classnames from 'classnames';
 
 import './MoviesCard.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function MoviesCard(props) {
+  const [isActive, setActive] = useState(props.saved);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
   return (
     <li className={classnames('movies-card', props.className)}>
       <div className='movies-card__info'>
@@ -12,9 +17,9 @@ export default function MoviesCard(props) {
           <h2 className='movies-card__title'>{props.title}</h2>
           <p className='movies-card__duration'>{props.duration}</p>
         </div>
-        <button className='movies-card__button' />
+        <button className={classnames('movies-card__button', isActive ? 'movies-card__button_saved': null )} onClick={toggleClass} />
       </div>
-      <img src={props.image} className='movies-card__info' alt={props.title}/>
+      <img src={props.image} className='movies-card__info' alt={props.title} />
     </li>
   )
 }
