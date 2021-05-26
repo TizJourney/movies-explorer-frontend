35ad6509c132f525d6dbd933b3397b8a475496be
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-26 18:01:40 +0300

    Добавить функиональность preloader'а

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -2,12 +2,37 @@ import classnames from 'classnames';
 
 import './Preloader.css';
 
-import React from 'react';
+import React, { useState } from 'react';
+
+// export default function Preloader(props) {
+//   return (
+
+//     <div className={classnames(props.className, 'preloader')}>
+//       <button className='preloader__button'>Ещё</button>
+//     </div>
+
+//   )
+// }
 
 export default function Preloader(props) {
+  const [isActive, setActive] = useState(false);
+
+  const activatePreload = () => {
+    setActive(!isActive);
+  };
+
   return (
     <div className={classnames(props.className, 'preloader')}>
-          <button className='preloader__button'>Ещё</button>
+      {isActive &&
+      <div className="preloader__loader">
+        <div className="preloader__container">
+          <span className="preloader__round"></span>
+        </div>
+      </div>
+      }
+      <div>
+        <button className='preloader__button' onClick={activatePreload}>Ещё</button>
+      </div>
     </div>
   )
 }
