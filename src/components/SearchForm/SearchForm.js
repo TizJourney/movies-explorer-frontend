728caeacf020fa784222ddbfb7e0733a4b884559
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-26 16:00:57 +0300

    первая реализация кнопки сабмита формы

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,12 +2,32 @@ import classnames from 'classnames';
 
 import './SearchForm.css';
 
+import search_button from '../../images/search-button.png';
+
 import React from 'react';
+
+function FilterCheckbox(props) {
+  return (
+    <div className='search-form__filter-container'>
+      <p className='search-form__widget-title'>Короткометражки</p>
+      <input type="checkbox" className='search-form__filter-checkbox'></input>
+    </div>
+  )
+}
+
+function SearchFormButton(props) {
+  return (
+    <button className='search-form__submit-button' />
+  )
+}
+
 
 export default function SearchForm(props) {
   return (
     <div className={classnames('search-form', props.className)}>
-      <p className='search-form__content'>search-form Placeholder</p>
+      <p className='search-form__content'>search-form Left Block</p>
+      <SearchFormButton />
+      <FilterCheckbox />
     </div>
   )
 }
