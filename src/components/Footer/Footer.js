9bc74dd8e75ded69279b9c2802b6eed5ae68ddd3
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-28 00:58:17 +0300

    адаптация главной страницы под среднее разрешение

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,9 +11,9 @@ export default React.memo(function Footer(props) {
       <div className="footer__text-container">
         <p className="footer__text">&copy; 2021</p>
         <nav className="footer__link-block">
-          <a href='https://www.facebook.com/konstantin.bondar.7' className='footer__link' target='_blank' rel='noreferrer'>Facebook</a>
-          <a href='https://github.com/TizJourney' className='footer__link' target='_blank' rel='noreferrer'>Github</a>
           <a href='https://praktikum.yandex.ru' className='footer__link' target='_blank' rel='noreferrer'>Яндекс.Практикум</a>
+          <a href='https://github.com/TizJourney' className='footer__link' target='_blank' rel='noreferrer'>Github</a>
+          <a href='https://www.facebook.com/konstantin.bondar.7' className='footer__link' target='_blank' rel='noreferrer'>Facebook</a>
         </nav>
       </div>
     </footer>
