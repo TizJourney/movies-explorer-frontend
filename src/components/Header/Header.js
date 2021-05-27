b3cb63ff95c8dab15045d8f4890fb3e5927ce2f0
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-27 17:15:36 +0300

    Реализовать заголовок с авторизацией

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,18 +5,30 @@ import './Header.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import main_logo from '../../images/main-logo.png';
+import account_logo from '../../images/account-logo.svg';
+
+function AccountLogo(props) {
+  return (
+    <div className={classnames(props.className, 'account-logo')}>
+      <p className='account-logo__text'>Аккаунт</p>
+      <img className='account-logo__logo' src={account_logo} alt='Изображение лого аккаунта'/>
+    </div>
+  )
+}
+
 export default function Header(props) {
 
-  const linkClassName = `header__link ${props.darkTheme && 'header__link_dark'}`;
+  const extraLinkClassName = props.darkTheme ? 'header__link_dark' : null;
 
   return (
     <header className={classnames(props.className, 'header')}>
-      <Link to='profile' className={linkClassName}>profile</Link>
-      <Link to='signin' className={linkClassName}>login</Link>
-      <Link to='signup' className={linkClassName}>register</Link>
-      <Link to='saved-movies' className={linkClassName}>saved-movies</Link>
-      <Link to='movies' className={linkClassName}>movies</Link>
-      <Link to='/' className={linkClassName}>main</Link>
+      <Link to='/' className={'header__link header__link_main'}><img src={main_logo} alt='лого главной страницы' className='header__main-logo'/></Link>
+      <nav className='header__links-block'>
+        <Link to='profile' className={classnames('header__link header__link_account', extraLinkClassName)}><AccountLogo className='header__account-logo'/></Link>
+        <Link to='movies' className={classnames('header__link header__link_movies',extraLinkClassName)}>Фильмы</Link>
+        <Link to='saved-movies'className={classnames('header__link header__link_saved-movies',  extraLinkClassName)}>Сохранённые фильмы</Link>
+      </nav>
     </header>
   )
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -10,7 +10,7 @@ export function ProfileInput(props) {
   const [inputValue, setInputValue] = useState(props.value);
 
   return (
-    <div  className={classnames('profile-input', props.className)}>
+    <div className={classnames('profile-input', props.className)}>
       <div className='profile-input__container' >
         <p className='profile-input__title'>{props.title}</p>
         <input className='profile-input__input' defaultValue={inputValue} value={inputValue.value} onChange={e => setInputValue(e.target.value)} />
@@ -23,14 +23,16 @@ export function ProfileInput(props) {
 export default function Login(props) {
   return (
     <div className='profile'>
-      <Header className='profile__header'/>
       <div className='profile__content'>
-        <h2 className='profile__title'>Привет, Константин!</h2>
-        <ProfileInput className='profile__input profile__input_bottom-divider' title='E-mail' value='Константин' />
-        <ProfileInput className='profile__input' title='Пароль' value='example@mail.ru' />
-        <button className='profile__button'>Редактировать</button>
-        <button className='profile__button profile__button_logout'>Выйти из аккаунт</button>
+        <Header className='profile__header' />
+        <div className='profile__body'>
+          <h2 className='profile__title'>Привет, Константин!</h2>
+          <ProfileInput className='profile__input profile__input_bottom-divider' title='E-mail' value='Константин' />
+          <ProfileInput className='profile__input' title='Пароль' value='example@mail.ru' />
+          <button className='profile__button'>Редактировать</button>
+          <button className='profile__button profile__button_logout'>Выйти из аккаунт</button>
+        </div>
       </div>
-    </div>
+    </div >
   )
 }
