31e7747e0ef45dc074aa3976d04a8b683e4c12a8
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-27 14:21:06 +0300

    Сделать базовую версию профиля

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,36 @@
+import classnames from 'classnames';
+
 import './Profile.css';
 
-import React from 'react';
+import Header from '../Header/Header';
 
-import SitePage from '../SitePage/SitePage';
+import React, { useState } from 'react';
 
-export default function Profile(props) {
+export function ProfileInput(props) {
+  const [inputValue, setInputValue] = useState(props.value);
+
   return (
-    <SitePage>
-      <div className="profile">
-        <h2>Profile placeholder</h2>
+    <div  className={classnames('profile-input', props.className)}>
+      <div className='profile-input__container' >
+        <p className='profile-input__title'>{props.title}</p>
+        <input className='profile-input__input' value={inputValue.value} onChange={e => setInputValue(e.target.value)} />
       </div>
-    </SitePage>
+      <p className='profile-input__input-error'>Текст ошибки</p>
+    </div>
+  )
+}
+
+export default function Login(props) {
+  return (
+    <div className='profile'>
+      <Header className='profile__header'/>
+      <div className='profile__content'>
+        <h2 className='profile__title'>Привет, Константин!</h2>
+        <ProfileInput className='profile__input profile__input_bottom-divider' title='E-mail' value='Константин' />
+        <ProfileInput className='profile__input' title='Пароль' value='example@mail.ru' />
+        <button className='profile__button'>Редактировать</button>
+        <button className='profile__button profile__button_logout'>Выйти из аккаунт</button>
+      </div>
+    </div>
   )
 }
