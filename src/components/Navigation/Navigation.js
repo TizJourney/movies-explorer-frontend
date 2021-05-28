1b044524894af3b37f63769d214b12ebc8845ffa
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-28 18:16:30 +0300

    Реализовать оверлей для меню

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -10,8 +10,8 @@ export default function Navigation(props) {
   return (
     <div className={`navigation ${props.isOpen && "navigation_opened"}`}>
       <div className='navigation__overlay' ></div>
-      <button className='navigation__close-button' onClick={() => {props.handleClose()} } />
       <div className='navigation__internal-content' >
+        <button className='navigation__close-button' onClick={() => {props.handleClose()} } />
         <nav className='navigation__links-block header__links-block_auth'>
           <button className='navigation__link navigation__link_main' onClick={() => {props.handleClick('/')} }>Главная</button>
           <button className='navigation__link navigation__link_movies' onClick={() => {props.handleClick('movies')} }> Фильмы</button>
