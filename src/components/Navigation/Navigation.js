44203423eb8d66a72fe5c857d2d751a88068cfc4
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-28 06:16:31 +0300

    Первый подход к попапу навигации

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,12 +8,14 @@ import AccountButton from '../AccountButton/AccountButton.js';
 export default function Navigation(props) {
   return (
     <div className={`navigation ${props.isOpen && "navigation_opened"}`}>
-      <nav className='navigation__links-block header__links-block_auth'>
-        <Link to='/' className='navigation__link navigation__link_main'>Главная</Link>
-        <Link to='movies' className='navigation__link navigation__link_movies'>Фильмы</Link>
-        <Link to='saved-movies' className='navigation__link hnavigation__link_saved-movies'>Сохранённые фильмы</Link>
-      </nav>
-      <Link to='profile' className='navigation__link navigation__link_account'><AccountButton className='header__account-logo' /></Link>
-    </div>
+      <div className='navigation__internal-content' >
+        <nav className='navigation__links-block header__links-block_auth'>
+          <button className='navigation__link navigation__link_main'>Главная</button>
+          <button to='movies' className='navigation__link navigation__link_movies'>Фильмы</button>
+          <button to='saved-movies' className='navigation__link hnavigation__link_saved-movies'>Сохранённые фильмы</button>
+        </nav>
+        <button to='profile' className='navigation__link navigation__link_account'><AccountButton className='header__account-logo' /></button>
+      </div>
+    </div >
   )
 }
