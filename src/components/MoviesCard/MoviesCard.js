3467cb62c8215db996039cfab1e9c005d713fe2c
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-29 23:23:28 +0300

    Переделать состояния карточек

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -6,9 +6,9 @@ import React, { useState } from 'react';
 
 export default function MoviesCard(props) {
   const [isActive, setActive] = useState(props.saved);
+  const [isHovored, setIsHovered] = useState(false);
 
-  const activeClassName = props.savedMode ? 'movies-card__button_saved-mode_active' : 'movies-card__button_normal-mode_active';
-  const disableClassName = props.savedMode ? 'movies-card__button_saved-mode_disable' : 'movies-card__button_normal-mode_disable';
+  const extraButtonClassName = isHovored ? (isActive ? 'movies-card__button_remove' : 'movies-card__button_save') : (isActive && !props.savedMode ? 'movies-card__button_saved' : null);
 
   const toggleClass = () => {
     setActive(!isActive);
@@ -16,9 +16,12 @@ export default function MoviesCard(props) {
 
   return (
     <li className={classnames('movies-card', props.className)}>
-      <div className='movies-card__image-container'>
+      <div className='movies-card__image-container'
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        >
         <img src={props.image} className='movies-card__image' alt={props.title} />
-        <button className={classnames('movies-card__button', isActive ? disableClassName : activeClassName )} onClick={toggleClass} />
+        <button className={classnames('movies-card__button', extraButtonClassName )} onClick={toggleClass} />
       </div>
       <div className='movies-card__text-blocks'>
           <h2 className='movies-card__title'>{props.title}</h2>
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -11,14 +11,15 @@ import React from 'react';
 export default function MoviesCardList(props) {
   return (
     <ul className={classnames('movies-card-list', props.className)}>
-      <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/>
+      { !props.savedMode && <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/> }
       <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode} saved={true}/>
-      <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/>
-      <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/>
+      { !props.savedMode && <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/> }
+      { !props.savedMode && <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/> }
+      { !props.savedMode && <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/> }
       <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode} saved={true}/>
-      <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/>
+      { !props.savedMode && <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/> }
       <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode} saved={true}/>
-      <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/>
+      { !props.savedMode && <MoviesCard className='movies-card-list__card' title='33 слова о дизайне' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/> }
       <MoviesCard className='movies-card-list__card' title='Очень длинное название фильма, которое точно не поместится' duration='1ч 47м' image={imageFilmExample} savedMode={props.savedMode}/>
     </ul>
   )
