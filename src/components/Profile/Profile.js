58287864a42dd7f2b5130a0f0432dc2b6e9255c8
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-30 00:52:58 +0300

    сделать валидацию для аккаунта

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,41 +1,65 @@
-import classnames from 'classnames';
-
 import './Profile.css';
 
 import Header from '../Header/Header';
 
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from "react-hook-form";
 
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 
-export function ProfileInput(props) {
-  const [inputValue, setInputValue] = useState(props.value);
-
-  return (
-    <div className={classnames('profile-input', props.className)}>
-      <div className='profile-input__container' >
-        <p className='profile-input__title'>{props.title}</p>
-        <input className='profile-input__input' defaultValue={inputValue} value={inputValue.value} onChange={e => setInputValue(e.target.value)} />
-      </div>
-      <p className='profile-input__input-error'>Текст ошибки</p>
-    </div>
-  )
-}
-
 export default function Profile(props) {
   const userContext = React.useContext(CurrentUserContext);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values);
+    //props.handleEditProfile();
+  };
 
   return (
     <div className='profile'>
       <div className='profile__content'>
         <Header className='profile__header' />
-        <div className='profile__body'>
+        <form name='profileForm' className='profile__body' onSubmit={handleSubmit(onSubmit)}>
           <h2 className='profile__title'>Привет, {userContext.name}!  </h2>
-          <ProfileInput className='profile__input profile__input_bottom-divider' title='Имя' value={userContext.name} />
-          <ProfileInput className='profile__input' title='E-mail' value={userContext.email} />
-          <button className='profile__button' onClick={props.handleEditProfile}>Редактировать</button>
+
+          <div className='profile__input-container' >
+            <p className='profile__input-title'>Имя</p>
+            <input
+              type='text'
+              className='profile__input'
+              name='name'
+              defaultValue={userContext.name}
+              {...register('name',
+                {
+                  required: { value: true, message: 'Поле обязательно для заполнения' },
+                  minLength: { value: 3, message: 'Минимальная длина поля 3' },
+                  maxLength: { value: 30, message: 'Максимальная длина поля 30' }
+                })
+              }
+            />
+            {errors.name && <p className='profile__input-error'>{errors.name.message}</p>}
+          </div>
+
+
+          <div className='profile__input-container' >
+            <p className='profile__input-title'>E-mail</p>
+            <input
+              type='email'
+              className='profile__input'
+              name='email'
+              defaultValue={userContext.email}
+              {...register('email',
+                {
+                  required: { value: true, message: 'Поле обязательно для заполнения' },
+                })
+              }
+            />
+            {errors.email && <p className='profile__input-error'>{errors.email.message}</p>}
+          </div>
+          <button type="submit" className='profile__button'>Редактировать</button>
           <button className='profile__button profile__button_logout' onClick={props.handleLogout}>Выйти из аккаунта</button>
-        </div>
+        </form>
       </div>
     </div >
   )
