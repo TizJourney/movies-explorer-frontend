126bedcfc87a7e8fce908039fb037ad9e61e6151
Konstantin Bondar <kostya.bondar@gmail.com>
2021-05-30 13:36:00 +0300

    ещё один вариант для нажатия на trailer

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -18,14 +18,15 @@ export default function MoviesCard(props) {
 
   return (
     <li className={classnames('movies-card', props.className)}>
-      <button className='movies-card__image-container'
+      <div className='movies-card__image-container'
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        onClick={handleClick}
         >
-        <img src={props.image} className='movies-card__image' alt={props.title} />
+        <button className='movies-card__image-button'>
+          <img src={props.image} className='movies-card__image' alt={props.title} onClick={handleClick} />
+        </button>
         <button className={classnames('movies-card__button', extraButtonClassName )} onClick={toggleClass} />
-      </button>
+      </div>
       <div className='movies-card__text-blocks'>
           <h2 className='movies-card__title'>{props.title}</h2>
           <p className='movies-card__duration'>{props.duration > 60 && `${Math.floor(props.duration /60)} ч`} {`${Math.floor(props.duration % 60)} м`} </p>
