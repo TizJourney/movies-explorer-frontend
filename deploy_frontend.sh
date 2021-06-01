7dbce2814dbbe5f4d92c949ab31e10a10668fcdf
Konstantin Bondar <kostya.bondar@gmail.com>
2021-06-01 04:25:10 +0300

    Переход на backend версию сайта

--- deploy_frontend.sh
+++ deploy_frontend.sh
@@ -1,6 +1,7 @@
 npm run build
-cd ./build
-echo "REACT_APP_API_URL=//api.tizjourney-films.nomo.nomoredomains.monster" > .env
-cd ..
 scp -r ./build/* kostya@84.252.134.210:/home/kostya/frontend
+
+# ssh kostya@84.252.134.210
+# #echo "REACT_APP_API_URL=//api.tizjourney-films.nomo.nomoredomains.monster" > .env
+# sudo systemctl restart nginx
 
